2d35940be0ce12f0fbd66ae8e56fbf616988e52e
Adrean Cebola <adreancebola@hotmail.com>
2016-10-13 22:53:21 -0300

    Alterações no método de penalidade

--- penalty_method.jl
+++ penalty_method.jl
@@ -1,13 +1,15 @@
-using NLPModels, Krylov
-function penalty_method(nlp::AbstractNLPModel; α = 0.5, tol = 1e-5, max_iter = 1000, max_time = 60)
+using Krylov, CUTEst, LinearOperators, NLPModels
+function penalty_method(nlp::AbstractNLPModel; α = 0.5, mem = 5, tol = 1e-5, max_iter = 1000, max_time = 60)
+
     exit_flag = 0
     x0 = nlp.meta.x0
+    n = nlp.meta.nvar
     f(x) = obj(nlp, x)
     c(x) = cons(nlp, x)
     ∇f(x) = grad(nlp, x)
     J(x) = jac(nlp, x)
+    μ = 0.1
     j = 0
-    μ = 1.0
 
     x = copy(x0) # Cópia de x0
     iter = 0
@@ -20,39 +22,50 @@ function penalty_method(nlp::AbstractNLPModel; α = 0.5, tol = 1e-5, max_iter = 
     A = J(x)
     m = length(cx)
     u = ones(m)
-    λ = u-(cx/μ)
-    L_xλ = ∇fx - A'*λ
-    H_Φ(x,λ) = hess(nlp, x;y=λ)
-    Φ(x,u,μ) = f(x) - dot(u,cx) + (0.5/μ)*(norm(cx, 2))^2
+    λ = u - (cx / μ)
+    L_xλ = ∇fx - A' * λ
+    Φ(x,u,μ) = f(x) - dot(u,cx) + (0.5 / μ) * (norm(cx, 2))^2
+    B = LBFGSOperator(n)
+    M = hess(nlp, x, y = λ) + (1 / μ) * (A' * A)
 
     while norm(L_xλ) > tol || norm(cx) > tol
 
-        B = H_Φ(x,λ) + (1/μ)*(A'A)
-        d,stats = cg(B,-L_xλ)
-        ∇fx_dot_d = dot(L_xλ,d)
+        ϵ_j = μ^(j + 1)
+        while norm(L_xλ) > ϵ_j
 
-        t = 1.0
-        while Φ(x + t*d,u,μ) > Φ(x,u,μ) + α*t*∇fx_dot_d
-            t = t*0.9
-            if t >= tol
-                break
+            d,stats = cg(M, -L_xλ)
+            ∇fx_dot_d = dot(L_xλ,d)
+            t = 1.0
+            while Φ(x + t*d,u,μ) > Φ(x,u,μ) + α*t*∇fx_dot_d
+                t = t*0.9
+                if t >= tol
+                    break
+                end
             end
-        end
 
-        x = x + t*d
-        fx = f(x)
-        ∇fx = ∇f(x)
-        A = J(x)
-        cx = c(x)
-        λ = u-(cx/μ)
-        L_xλ = ∇fx - A'*λ
-        η = μ^(0.1+0.9*j)
-        if norm(cx, 2) <= η
-            u = λ
-        else
-            μ = max(min(0.1,sqrt(μ))*μ,1.0)
-            j+=1
+            s = t * d
+            x = x + s
+            y = ∇f(x) - ∇fx
+            A = J(x)
+            B = push!(B, s, y)
+            M = B + (1 / μ) * (A'*A)
+            fx = f(x)
+            ∇fx = ∇f(x)
+            cx = c(x)
+            λ = u - (cx / μ)
+            L_xλ = ∇fx - A' * λ
+            η = μ^(0.1 + 0.9 * j)
+            ϵ_j = μ^(j + 1)
+
+            if norm(cx) <= η
+                u = λ
+            else
+                μ = max(min(0.1, sqrt(μ)) * μ,1e-8)
+                j+=1
+            end
+
         end
+        #println("μ=$μ")
 
         iter = iter + 1
         if iter >= max_iter
