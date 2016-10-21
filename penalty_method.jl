4840bffb2538ba4a8a487445ccd5ed7816403bf1
Adrean Cebola <adreancebola@hotmail.com>
2016-10-20 23:14:59 -0200

    Executei os testes, deu 77% nos médios

--- penalty_method.jl
+++ penalty_method.jl
@@ -1,6 +1,30 @@
 using Krylov, CUTEst, LinearOperators, NLPModels
+
+function SOC(Φ, x, d, L_xλ, A, u, c, μ)
+    η1,η2 = 0.2,0.5
+    t = 1.0
+    cond = true
+    while cond == true
+        d1,stats = cg((A * A'),c(x + d))
+        d1 = -A' * d1
+        if Φ(x + d + d1,u,μ) <= Φ(x, u, μ) + 0.5 * t * dot(L_xλ, d)
+            d = d + d1
+            x = x + d
+            cond = false
+        else
+            t = t * 0.9
+        end
+        if t <= 1e-16
+            break
+        end
+
+    end
+    return x, d
+end
+
 function penalty_method(nlp::AbstractNLPModel; α = 0.5, tol = 1e-5, max_iter = 1000, max_time = 60, verbose = false)
 
+    k_max = 1500
     exit_flag = 0
     x = nlp.meta.x0
     n = nlp.meta.nvar
@@ -33,20 +57,20 @@ function penalty_method(nlp::AbstractNLPModel; α = 0.5, tol = 1e-5, max_iter = 
     η = μ^(0.1 + 0.9 * j)
 
     while norm(L_x) > tol || norm(cx) > tol
-
+        k = 0
         while norm(L_xλ) > ϵ_j
-
             d,stats = cg(M, -L_xλ)
             t = 1.0
-
-            while Φ(x + t * d,u,μ) > Φ(x,u,μ) + α * t * dot(L_xλ,d)
+            Φ_x = Φ(x,u,μ)
+            L_xλ_d = dot(L_xλ,d)
+            while Φ(x + t * d,u,μ) > Φ_x + α * t * L_xλ_d
                 t = t * 0.9
                 if t <= 1e-16
                     break
                 end
             end
-            if Φ(x + t * d,u,μ) > Φ(x,u,μ) + α * t * dot(L_xλ,d)
-                x1, d1 = SOC(Φ, x, d, L_xλ, J, u, c, μ)
+            if Φ(x + t * d,u,μ) > Φ_x + α * t * L_xλ_d
+                x1, d1 = SOC(Φ, x, d, L_xλ, A, u, c, μ)
                 A = J(x1)
                 cx = c(x1)
                 ∇fx = ∇f(x1)
@@ -65,8 +89,11 @@ function penalty_method(nlp::AbstractNLPModel; α = 0.5, tol = 1e-5, max_iter = 
                 L_xλ = ∇fx - A' * λ
             end
             M = push!(B, t * d, ∇f(x) - ∇fx) + (1 / μ) * (A' * A)
+            k += 1
+            if k >= k_max
+                break
+            end
         end
-        #println("cx=$(norm(cx)),L_x=$(norm(L_x))")
         L_x = ∇fx - A' * u
         if norm(L_xλ) <= ϵ_j
             ϵ_j = ϵ_j*0.1
@@ -92,6 +119,5 @@ function penalty_method(nlp::AbstractNLPModel; α = 0.5, tol = 1e-5, max_iter = 
             break
         end
     end
-    return x, f(x), norm(L_x),
-    norm(cx), exit_flag, iter, elapsed_time
+    return x, f(x), norm(L_x),norm(cx), exit_flag, iter, elapsed_time
 end
