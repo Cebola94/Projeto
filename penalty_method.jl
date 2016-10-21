ec36b4dd084f54d3380af74dc84bccb26f0f55a4
Cebola94 <adreancebola@hotmail.com>
2016-10-20 23:17:32 -0200

    Agora vai kkkkkk

--- penalty_method.jl
+++ penalty_method.jl
@@ -14,19 +14,16 @@ function SOC(Φ, x, d, L_xλ, A, u, c, μ)
         else
             t = t * 0.9
         end
-<<<<<<< HEAD
         if t <= 1e-16
             break
         end
-=======
->>>>>>> 1f9052f77b56f4e5553fb6975c7e863f2d8a67ff
-
+        
     end
     return x, d
 end
 
 function penalty_method(nlp::AbstractNLPModel; α = 0.5, tol = 1e-5, max_iter = 1000, max_time = 60, verbose = false)
-
+    
     k_max = 1500
     exit_flag = 0
     x = nlp.meta.x0
@@ -35,10 +32,10 @@ function penalty_method(nlp::AbstractNLPModel; α = 0.5, tol = 1e-5, max_iter = 
     c(x) = cons(nlp, x)
     ∇f(x) = grad(nlp, x)
     J(x) = jac(nlp, x)
-
+    
     μ = 10.0
     j = 0
-
+    
     iter = 0
     start_time = time()
     elapsed_time = 0.0
@@ -58,12 +55,12 @@ function penalty_method(nlp::AbstractNLPModel; α = 0.5, tol = 1e-5, max_iter = 
     M = hess(nlp, x, y = λ) + (1 / μ) * (A' * A)
     ϵ_j = μ^(j + 1)
     η = μ^(0.1 + 0.9 * j)
-
+    
     while norm(L_x) > tol || norm(cx) > tol
         k = 0
-        while norm(L_xλ) > ϵ_j
+        while norm(L_xλ) > ϵ_j         
             d,stats = cg(M, -L_xλ)
-            t = 1.0
+            t = 1.0           
             Φ_x = Φ(x,u,μ)
             L_xλ_d = dot(L_xλ,d)
             while Φ(x + t * d,u,μ) > Φ_x + α * t * L_xλ_d
@@ -83,7 +80,7 @@ function penalty_method(nlp::AbstractNLPModel; α = 0.5, tol = 1e-5, max_iter = 
                     t = 1.0
                     x, d = x1, d1
                 end
-            else
+            else                  
                 x = x + t * d
                 A = J(x)
                 ∇fx = ∇f(x)
@@ -92,7 +89,7 @@ function penalty_method(nlp::AbstractNLPModel; α = 0.5, tol = 1e-5, max_iter = 
                 L_xλ = ∇fx - A' * λ
             end
             M = push!(B, t * d, ∇f(x) - ∇fx) + (1 / μ) * (A' * A)
-            k += 1
+            k += 1 
             if k >= k_max
                 break
             end
@@ -101,7 +98,7 @@ function penalty_method(nlp::AbstractNLPModel; α = 0.5, tol = 1e-5, max_iter = 
         if norm(L_xλ) <= ϵ_j
             ϵ_j = ϵ_j*0.1
         else
-            ϵ_j = μ^(j + 1)
+            ϵ_j = μ^(j + 1)  
         end
         η = μ^(0.1 + 0.9 * j)
         if norm(cx) <= η
